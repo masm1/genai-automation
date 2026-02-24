b357568173fa0d699807f538c86af05c46bac211
Sumit Kumar <mviesk@gmail.com>
2026-02-25 03:55:15 +0530

    Files have been modified

--- src/test/java/base/DriverFactory.java
+++ src/test/java/base/DriverFactory.java
@@ -3,14 +3,35 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import utils.ConfigReader;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverFactory {
 
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static void initDriver(String browser) {
-        WebDriverManager.chromedriver().setup();
-        driver.set(new ChromeDriver());
+        if(ConfigReader.getProperty("grid").equals("true")) {
+
+            try {
+                driver.set(new RemoteWebDriver(
+                        new URL(ConfigReader.getProperty("gridUrl")),
+                        new ChromeOptions()
+                ));
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+
+            if(browser.equalsIgnoreCase("chrome")) {
+                driver.set(new ChromeDriver());
+            }
+        }
     }
 
     public static WebDriver getDriver() {
